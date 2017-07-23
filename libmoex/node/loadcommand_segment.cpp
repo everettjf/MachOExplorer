b9bd674d73776db45629697086b7a91a55a9b92d
everettjf <everettjf@live.com>
2017-07-24 00:45:22 +0800

    load cmds

--- libmoex/node/loadcommand_segment.cpp
+++ libmoex/node/loadcommand_segment.cpp
@@ -5,6 +5,39 @@
 
 MOEX_NAMESPACE_BEGIN
 
+void LoadCommand_LC_SEGMENT::Init(void * offset,NodeContextPtr & ctx){
+    LoadCommandImpl::Init(offset,ctx);
+
+    for(uint32_t idx = 0; idx < cmd_->nsects; ++idx){
+        section * cur = reinterpret_cast<section*>((char*)offset_ + data_size_cmd + idx * sizeof(section));
+
+        MachSectionPtr section = std::make_shared<MachSection>();
+        section->Init(cur,ctx);
+        sections_.push_back(section);
+    }
+}
+
+std::string LoadCommand_LC_SEGMENT::GetShortCharacteristicDescription(){
+    return segment_name();
+}
+
+
+
+void LoadCommand_LC_SEGMENT_64::Init(void * offset,NodeContextPtr & ctx){
+    LoadCommandImpl::Init(offset,ctx);
+
+    for(uint32_t idx = 0; idx < cmd_->nsects; ++idx){
+        section * cur = reinterpret_cast<section*>((char*)offset_ + data_size_cmd + idx * sizeof(section_64));
+
+        MachSection64Ptr section = std::make_shared<MachSection64>();
+        section->Init(cur,ctx);
+        sections_.push_back(section);
+    }
+}
+
+std::string LoadCommand_LC_SEGMENT_64::GetShortCharacteristicDescription(){
+    return segment_name();
+}
 
 MOEX_NAMESPACE_END
 
--- libmoex/node/loadcommand_segment.h
+++ libmoex/node/loadcommand_segment.h
@@ -16,46 +16,26 @@ protected:
     std::vector<MachSectionPtr> sections_;
 public:
     std::vector<MachSectionPtr> & sections_ref(){return sections_;}
-
     std::string segment_name()const{ return std::string(cmd_->segname,16).c_str();}
 
-    void Init(void * offset,NodeContextPtr & ctx)override {
-        LoadCommandImpl::Init(offset,ctx);
-
-        for(uint32_t idx = 0; idx < cmd_->nsects; ++idx){
-            section * cur = reinterpret_cast<section*>((char*)offset_ + data_size_cmd + idx * sizeof(section));
-
-            MachSectionPtr section = std::make_shared<MachSection>();
-            section->Init(cur,ctx);
-            sections_.push_back(section);
-        }
-    }
+    void Init(void * offset,NodeContextPtr & ctx)override ;
 
     std::string GetTypeName() override{ return "segment_command";}
+
+    std::string GetShortCharacteristicDescription()override;
 };
 
 class LoadCommand_LC_SEGMENT_64 : public LoadCommandImpl<segment_command_64>{
 protected:
     std::vector<MachSection64Ptr> sections_;
 public:
-
     std::string segment_name()const{ return std::string(cmd_->segname,16).c_str();}
-
     std::vector<MachSection64Ptr> & sections_ref(){return sections_;}
 
-    void Init(void * offset,NodeContextPtr & ctx)override {
-        LoadCommandImpl::Init(offset,ctx);
-
-        for(uint32_t idx = 0; idx < cmd_->nsects; ++idx){
-            section * cur = reinterpret_cast<section*>((char*)offset_ + data_size_cmd + idx * sizeof(section_64));
-
-            MachSection64Ptr section = std::make_shared<MachSection64>();
-            section->Init(cur,ctx);
-            sections_.push_back(section);
-        }
-    }
+    void Init(void * offset,NodeContextPtr & ctx)override ;
 
     std::string GetTypeName() override{ return "segment_command_64";}
+    std::string GetShortCharacteristicDescription()override;
 };
 
 MOEX_NAMESPACE_END
