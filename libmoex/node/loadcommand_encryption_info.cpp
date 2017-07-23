fce25f4a9e1ab23a34d99d508004d0102afe9609
everettjf <everettjf@live.com>
2017-07-24 00:48:52 +0800

    cryptid

--- libmoex/node/loadcommand_encryption_info.cpp
+++ libmoex/node/loadcommand_encryption_info.cpp
@@ -7,6 +7,13 @@
 
 MOEX_NAMESPACE_BEGIN
 
+std::string LoadCommand_LC_ENCRYPTION_INFO::GetShortCharacteristicDescription(){
+    return boost::str(boost::format("cryptid=%1%")%cmd()->cryptid);
+}
+
+std::string LoadCommand_LC_ENCRYPTION_INFO_64::GetShortCharacteristicDescription(){
+    return boost::str(boost::format("cryptid=%1%")%cmd()->cryptid);
+}
 
 MOEX_NAMESPACE_END
 
