2d7b1c7cf8f545764f88c3e007be3bfff9982aee
everettjf <everettjf@live.com>
2017-08-07 00:06:03 +0800

    data in code entries

--- libmoex/viewnode/DataInCodeEntriesViewNode.cpp
+++ libmoex/viewnode/DataInCodeEntriesViewNode.cpp
@@ -7,6 +7,25 @@
 MOEX_NAMESPACE_BEGIN
 
 void DataInCodeEntriesViewNode::InitViewDatas(){
+    using namespace moex::util;
+
+    moex::LoadCommand_LINKEDIT_DATA *seg=nullptr;
+    for(auto & cmd : mh_->loadcmds_ref()){
+        if(cmd->offset()->cmd == LC_DATA_IN_CODE) {
+            seg = static_cast<moex::LoadCommand_LINKEDIT_DATA*>(cmd.get());
+            break;
+        }
+    }
+    if(!seg)
+        return;
+
+    auto b = CreateBinaryViewDataPtr();
+    b->offset = (char*)mh_->header_start() + seg->cmd()->dataoff;
+    b->size = seg->cmd()->datasize;
+    AddViewData(b);
+
+
+    // Dices
 
 }
 
