00f49c4d802289694f587334af1146af85a68799
everettjf <everettjf@live.com>
2017-08-24 01:27:58 +0800

    place holders

--- libmoex/viewnode/DynamicLoaderInfoViewNode.cpp
+++ libmoex/viewnode/DynamicLoaderInfoViewNode.cpp
@@ -7,7 +7,127 @@
 MOEX_NAMESPACE_BEGIN
 
 
+void RebaseInfoViewNode::InitViewDatas()
+{
+    {
+        auto b = CreateBinaryViewDataPtr();
+        b->offset = 0;
+        b->size = 0;
+        AddViewData(b);
+    }
+    {
+        auto x = CreateTableViewDataPtr("Opcodes");
+        x->AddRow("//todo","","","");
+        AddViewData(x);
+    }
+    {
+        auto x = CreateTableViewDataPtr("Actions");
+        x->AddRow("//todo","","","");
+        AddViewData(x);
+    }
+}
 
+void BindingInfoViewNode::InitViewDatas()
+{
+
+    {
+        auto b = CreateBinaryViewDataPtr();
+        b->offset = 0;
+        b->size = 0;
+        AddViewData(b);
+    }
+    {
+        auto x = CreateTableViewDataPtr("Opcodes");
+        x->AddRow("//todo","","","");
+        AddViewData(x);
+    }
+    {
+        auto x = CreateTableViewDataPtr("Actions");
+        x->AddRow("//todo","","","");
+        AddViewData(x);
+    }
+}
+
+void WeakBindingInfoViewNode::InitViewDatas()
+{
+
+    {
+        auto b = CreateBinaryViewDataPtr();
+        b->offset = 0;
+        b->size = 0;
+        AddViewData(b);
+    }
+    {
+        auto x = CreateTableViewDataPtr("Opcodes");
+        x->AddRow("//todo","","","");
+        AddViewData(x);
+    }
+    {
+        auto x = CreateTableViewDataPtr("Actions");
+        x->AddRow("//todo","","","");
+        AddViewData(x);
+    }
+}
+
+void LazyBindingInfoViewNode::InitViewDatas()
+{
+
+    {
+        auto b = CreateBinaryViewDataPtr();
+        b->offset = 0;
+        b->size = 0;
+        AddViewData(b);
+    }
+    {
+        auto x = CreateTableViewDataPtr("Opcodes");
+        x->AddRow("//todo","","","");
+        AddViewData(x);
+    }
+    {
+        auto x = CreateTableViewDataPtr("Actions");
+        x->AddRow("//todo","","","");
+        AddViewData(x);
+    }
+}
+
+void ExportInfoViewNode::InitViewDatas()
+{
+
+    {
+        auto b = CreateBinaryViewDataPtr();
+        b->offset = 0;
+        b->size = 0;
+        AddViewData(b);
+    }
+    {
+        auto x = CreateTableViewDataPtr("Opcodes");
+        x->AddRow("//todo","","","");
+        AddViewData(x);
+    }
+    {
+        auto x = CreateTableViewDataPtr("Actions");
+        x->AddRow("//todo","","","");
+        AddViewData(x);
+    }
+}
+
+void DynamicLoaderInfoViewNode::ForEachChild(std::function<void(ViewNode*)> callback){
+    callback(rebase_.get());
+    callback(binding_.get());
+    callback(weak_binding_.get());
+    callback(lazy_binding_.get());
+    callback(export_.get());
+}
+
+void DynamicLoaderInfoViewNode::Init(MachHeaderPtr mh){
+    mh_ = mh;
+
+    rebase_ = std::make_shared<RebaseInfoViewNode>();
+    binding_ = std::make_shared<BindingInfoViewNode>();
+    weak_binding_ = std::make_shared<WeakBindingInfoViewNode>();
+    lazy_binding_ = std::make_shared<LazyBindingInfoViewNode>();
+    export_ = std::make_shared<ExportInfoViewNode>();
+}
 
 void DynamicLoaderInfoViewNode::InitViewDatas()
 {
@@ -17,5 +137,6 @@ void DynamicLoaderInfoViewNode::InitViewDatas()
         AddViewData(x);
     }
 }
+
 
 MOEX_NAMESPACE_END
--- libmoex/viewnode/DynamicLoaderInfoViewNode.h
+++ libmoex/viewnode/DynamicLoaderInfoViewNode.h
@@ -9,14 +9,51 @@
 
 MOEX_NAMESPACE_BEGIN
 
+class RebaseInfoViewNode : public ViewNode {
+public:
+    std::string GetDisplayName()override { return "Rebase Info";}
+    void InitViewDatas()override;
+};
+
+class BindingInfoViewNode : public ViewNode {
+public:
+    std::string GetDisplayName()override { return "Binding Info";}
+    void InitViewDatas()override;
+};
+
+class WeakBindingInfoViewNode : public ViewNode {
+public:
+    std::string GetDisplayName()override { return "Weak Binding Info";}
+    void InitViewDatas()override;
+};
+
+class LazyBindingInfoViewNode : public ViewNode {
+public:
+    std::string GetDisplayName()override { return "Lazy Binding Info";}
+    void InitViewDatas()override;
+};
+
+class ExportInfoViewNode : public ViewNode {
+public:
+    std::string GetDisplayName()override { return "Export Info";}
+    void InitViewDatas()override;
+};
+
 class DynamicLoaderInfoViewNode : public ViewNode{
 private:
     MachHeaderPtr mh_;
+
+    std::shared_ptr<RebaseInfoViewNode> rebase_;
+    std::shared_ptr<BindingInfoViewNode> binding_;
+    std::shared_ptr<WeakBindingInfoViewNode> weak_binding_;
+    std::shared_ptr<LazyBindingInfoViewNode> lazy_binding_;
+    std::shared_ptr<ExportInfoViewNode> export_;
 public:
-    void Init(MachHeaderPtr mh){mh_ = mh;}
+    void Init(MachHeaderPtr mh);
 public:
     std::string GetDisplayName()override { return "Dynamic Loader Info";}
     void InitViewDatas()override;
+    void ForEachChild(std::function<void(ViewNode*)> callback) override ;
 };
 using DynamicLoaderInfoViewNodePtr = std::shared_ptr<DynamicLoaderInfoViewNode>;
 
