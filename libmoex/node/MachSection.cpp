2690df9c3cccc5b7c0a791a60fe06d918965e005
everettjf <everettjf@live.com>
2017-10-23 00:08:08 +0800

    fix error

--- libmoex/node/MachSection.cpp
+++ libmoex/node/MachSection.cpp
@@ -7,21 +7,19 @@
 MOEX_NAMESPACE_BEGIN
 
 void MachSection::Init(section *offset,NodeContextPtr & ctx){
-    is64_ = false;
     sect_.Init(offset);
     section_ = std::make_shared<MachSectionInternal>();
     section_->Init(offset,ctx);
 }
 
 void MachSection::Init(section_64 *offset,NodeContextPtr & ctx){
-    is64_ = true;
     sect_.Init(offset);
     section64_ = std::make_shared<MachSection64Internal>();
     section64_->Init(offset,ctx);
 }
 
 uint64_t MachSection::GetRAW(const void *addr){
-    if(is64_)
+    if(Is64())
         return (uint64_t)addr - (uint64_t)section64_->ctx()->file_start;
     else
         return (uint64_t)addr - (uint64_t)section_->ctx()->file_start;
