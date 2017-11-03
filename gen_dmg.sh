0d3ee03346bce4ee0e14d4df50e487771fb84e41
everettjf <everettjf@live.com>
2017-11-04 02:08:29 +0800

    opt

--- gen_dmg.sh
+++ gen_dmg.sh
@@ -1,6 +1,9 @@
-rm ./build-moex-gui-Desktop_Qt_5_9_1_clang_64bit-Release/MOEX.dmg
+rm ./build-moex-gui-Desktop_Qt_5_9_1_clang_64bit-Release/MachOExplorer.dmg
 rm ./release/MachOExplorer.dmg
 
-~/qt/5.9.1/clang_64/bin/macdeployqt ./build-moex-gui-Desktop_Qt_5_9_1_clang_64bit-Release/MachOExplorer.app -dmg
 mkdir release
-cp ./build-moex-gui-Desktop_Qt_5_9_1_clang_64bit-Release/MachOExplorer.dmg ./release/
+cd ./build-moex-gui-Desktop_Qt_5_9_1_clang_64bit-Release/
+~/qt/5.9.1/clang_64/bin/macdeployqt ./MachOExplorer.app -dmg
+cp ./MachOExplorer.dmg ../release/MachOExplorer.dmg
+cd ../
+
