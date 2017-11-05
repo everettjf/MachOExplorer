3c86dc5bb574c67a7523333b1a01b62ff70eded1
everettjf <everettjf@live.com>
2017-11-05 23:56:57 +0800

    fix

--- dist_cli.sh
+++ dist_cli.sh
@@ -6,8 +6,8 @@ mkdir ./release-cli/package
 mkdir ./release-cli/package/bin
 cp ./moex-cli/cmake-build-release/moex ./release-cli/package/bin/moex
 
-cd ./release-cli/package/
-tar -zcvf ../moex.tar.gz .
-cd ../../
+cd ./release-cli/
+tar -zcvf ./moex.tar.gz ./package
+cd ../
 
 echo "finish create moex.tar.gz"
