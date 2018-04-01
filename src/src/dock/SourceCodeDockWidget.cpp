01d8b493583701d286ebbd2062dfd7b3ee7cb47e
everettjf <everettjf@live.com>
2018-04-01 23:25:23 +0800

    source code

--- src/src/dock/SourceCodeDockWidget.cpp
+++ src/src/dock/SourceCodeDockWidget.cpp
@@ -15,7 +15,25 @@ SourceCodeDockWidget::SourceCodeDockWidget(QWidget *parent) : QDockWidget(parent
     setWidget(textEdit);
 
     //default message
-    this->setContent(QStringLiteral("print \"hello world\""));
+    std::string code = R"(
+#define FAT_MAGIC	0xcafebabe
+#define FAT_CIGAM	0xbebafeca	/* NXSwapLong(FAT_MAGIC) */
+
+    struct fat_header {
+        uint32_t	magic;		/* FAT_MAGIC */
+        uint32_t	nfat_arch;	/* number of structs that follow */
+    };
+
+    struct fat_arch {
+        cpu_type_t	cputype;	/* cpu specifier (int) */
+        cpu_subtype_t	cpusubtype;	/* machine specifier (int) */
+        uint32_t	offset;		/* file offset to this object file */
+        uint32_t	size;		/* size of this object file */
+        uint32_t	align;		/* alignment as a power of 2 */
+    };
+
+                       )";
+    this->setContent(QString::fromStdString(code));
 }
 void SourceCodeDockWidget::setContent(const QString &line)
 {
